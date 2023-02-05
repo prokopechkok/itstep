b1fb5fc2a6c0f347d5c1ce583e2b385c1bc253e3
prokopechkok <104778176+prokopechkok@users.noreply.github.com>
2023-02-05 18:41:19 +0200

    deleted unneeded lines

--- find_unique_char.py
+++ find_unique_char.py
@@ -7,13 +7,11 @@ def find_unique_char(str):
 
     for item in words_list:
         for ch in item:
-            if item != " ":
-                freq = Counter(item)
-                if freq[ch] == 1:
-                    chars_list.append(ch)
-                    break
+            freq = Counter(item)
+            if freq[ch] == 1:
+                chars_list.append(ch)
             break
-        continue
+
     for char in chars_list:
         freq_2 = Counter(chars_list)
         if freq_2[char] == 1:
@@ -21,9 +19,11 @@ def find_unique_char(str):
             break
 
 
-str = 'The Tao gave birth to machine language.  Machine language gave birth to the assembler. The assembler gave ' \
+my_str = 'The Tao gave birth to machine language.  Machine language gave birth to the assembler. The assembler gave ' \
       'birth to the compiler.  Now there are ten thousand languages. Each language has its purpose, however humble.  ' \
       'Each language expresses the Yin and Yang of software.  Each language has its place within the Tao. But do not ' \
       'program in COBOL if you can avoid it. -- Geoffrey James, "The Tao of Programming"'
 
-find_unique_char(str)
+find_unique_char(my_str)
+find_unique_char("C makes it easy for you to shoot yourself in the foot. C++ makes that harder, but when you do, "
+                 "it blows away your whole leg. (с) Bjarne Stroustrup")
